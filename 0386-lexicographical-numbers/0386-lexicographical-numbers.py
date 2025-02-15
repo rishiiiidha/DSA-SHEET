1e966d90be38672d35e68c4d581d4f1027183bf4
Sri Venkata Rishidha Addanki <rishidha04@gmail.com>
2025-02-15 14:54:16 +0530

    Time: 20 ms (37.8%), Space: 23.1 MB (15.29%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        ans = []
+        for i in range(1,n+1):
+            ans.append(str(i))
+        ans.sort()
+        for i in range(n):
+            ans[i] = int(ans[i])
+        return ans
+        
